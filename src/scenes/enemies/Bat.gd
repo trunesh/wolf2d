e7312091086917951da983b61c0422fff156f039
trunesh loke <trunesh.loke123@gmail.com>
2023-02-25 00:36:04 +0530

    Bat wander AI controller

--- src/scenes/enemies/Bat.gd
+++ src/scenes/enemies/Bat.gd
@@ -21,6 +21,7 @@ onready var stats=$Stats
 onready var playerDetectionZone = $PlayerDetectionZone
 onready var hurtBox =$HurtBox
 onready var softCollision = $SoftCollision
+onready var wanderController = $WanderController
 
 
 func _physics_process(delta):
@@ -31,12 +32,20 @@ func _physics_process(delta):
 		IDLE:
 			velocity = velocity.move_toward(Vector2.ZERO,FRICTION*delta )
 			seek_player()
+			if wanderController.get_time_left()==0:
+				state = pick_random_state([IDLE,WANDER])
+				wanderController.start_wander_timer(rand_range(1,3))
 		WANDER:
-			pass
+			seek_player()
+			if wanderController.get_time_left()==0:
+				state = pick_random_state([IDLE,WANDER])
+				wanderController.start_wander_timer(rand_range(1,3))
+			var direction = global_position.direction_to(wanderController.target_position)
+			velocity = velocity.move_toward(direction * MAX_SPEED,ACCELERATION*delta)
 		CHASE:
 			var player = playerDetectionZone.player
 			if player!=null:
-				var direction = (player.global_position - global_position).normalized()
+				var direction = global_position.direction_to(player.global_position)
 				velocity = velocity.move_toward(direction * MAX_SPEED,ACCELERATION*delta)
 			else:
 				state = IDLE
@@ -50,6 +59,10 @@ func seek_player():
 	if playerDetectionZone.can_see_player():
 		state = CHASE 
 	
+func pick_random_state(state_list):
+	state_list.shuffle()
+	return state_list.pop_front()
+	 
 
 func _on_HurtBox_area_entered(area):
 	stats.health-=area.damage
